897a08750db035427b923fe9b4fe724dbfbb5064
zaddyphwithafatty <enzo.marin@ucas-edu.net>
2025-09-02 14:38:47 -0600

    finished variable practice

---
+++ practices/variables_practice.py
@@ -0,0 +1,22 @@
+#em 7th variable practice
+
+name = "my name is enzo"
+print (name)
+num_1 = "my a number i like between 1 and 10 is 1" 
+print(num_1)
+num_6 = "a number i preffer between 100 and 1000 is 670" 
+print(num_6)
+almond = "i had an almond for breakfast"
+print(almond)
+green = "y favorite color is green"
+print (green)
+ucas = "utah county acadamy of science"
+print (ucas)
+25 = "the year is 2025"
+print (25)
+brownish = "my ye color is brownish"
+print (brownish)
+14 = "my age is 14"
+print (14)
+church = "my facorite subject is seminary"
+print (church)
