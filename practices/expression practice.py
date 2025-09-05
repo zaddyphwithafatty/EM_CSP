320b612d446e510df04b9eb0342295fcb5fd64dd
zaddyphwithafatty <enzo.marin@ucas-edu.net>
2025-09-04 22:11:38 -0600

    Update expression practice.py

--- practices/expression practice.py
+++ practices/expression practice.py
@@ -1,8 +1,91 @@
-#Problem 1
+#problem 1
 a = 7
 b = 24
 c = 8
 d = 4
 e = 6
 result = a - b / c * d + e
-print(result)  # Output: 1.0
+print(result)  # output: 1.0
+
+#problem 2
+a = 18 
+b = 3
+c = 7
+d = 2
+e = 5
+result = a / b - c + d * e
+print(result) # output: 9.0
+
+#problem 3
+a = 6 
+b = 4
+c = 12
+d = 72
+e = 8
+f = 9
+result = a * b / c + d / e - f
+print(result) # output: 2.0
+
+#problem 4
+a = 17
+b = 6
+c = 2
+d = 4
+e = 3
+result = (a - b / c) + d * e 
+print(result) # output: 26
+
+#problem 5
+a = 2 
+b =  1 
+c =  4 
+d = 2
+e =2
+f = 6
+g = 2
+h = 3
+result = -(a * b * c - d / e) + (f + g - h)
+print(result) # output: -1
+
+#problem 6
+a = 1
+b = 3   
+c = 4
+d = 7
+e = 5
+f = 2
+g = 24
+h = 6
+result = -a*((b-c*d)/e) - f * g / h
+print(result) # output: -3
+
+#problem 7
+a = 3
+b = 5
+c = 15
+d = 5
+e = 2
+result = (a * b**2 / c) - (d - e**2)
+print(result) # output: 4
+
+
+#problem 8
+a = 1
+b = 2
+c = 3
+d = 2
+e = 4
+result = (a**4 * b**2 + c**3) - d**5 / e
+print(result) # output: 23
+
+
+#problem 9
+a = 22
+b = 2
+c = 2
+d = 5
+e = 4
+f = 6
+g =6
+result = (a / b - c * d)**2 + (e- f / g)**2
+print(result) # output: 10
