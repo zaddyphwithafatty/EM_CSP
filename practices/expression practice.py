e7ea949486878b100b924e307db3fa5564ac5534
zaddyphwithafatty <enzo.marin@ucas-edu.net>
2025-09-04 19:47:43 -0600

    expression practice.py

---
+++ practices/expression practice.py
@@ -0,0 +1,18 @@
+#problem 1
+
+a=7
+b=24
+c=8
+d=4
+e=6
+result=("a-b/c*d+e
+print("problem 1:", result1)
+#problem2
+
+a=18
+b=3
+c=7
+d=2
+e=5
+result2 = a / b - c + d * e
+print("problem2:", result2)
