35cff2e93fa2a20a8905809dd17eb7a7dca9fee8
zaddyphwithafatty <enzo.marin@ucas-edu.net>
2025-09-04 20:42:21 -0600

    Update expression practice.py

--- practices/expression practice.py
+++ practices/expression practice.py
@@ -1,18 +1,7 @@
-#problem 1
-
-a=7
-b=24
-c=8
-d=4
-e=6
-result=("a-b/c*d+e
-print("problem 1:", result1)
-#problem2
-
-a=18
-b=3
-c=7
-d=2
-e=5
-result2 = a / b - c + d * e
-print("problem2:", result2)
+Problem #1
+num = 7
+numTwo = 24
+numThree = 8
+numFourth = 4
+numFive = 6
+print((7-24)/8*4+6=1)
