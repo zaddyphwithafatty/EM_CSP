3ccdb976e59bb9eeb1f202f29ee9670a39f86c9a
zaddyphwithafatty <enzo.marin@ucas-edu.net>
2025-09-05 14:41:53 -0600

    calc homework

---
+++ practices/budget_calc.py
@@ -0,0 +1,5 @@
+# EM 7th calc
+
+# - income, rent, utlities, groceries, transportation saving is 10
+# spend = income -(utilities +rent -  groceries + traonsportation + savings)
+precent_rent = incomen * 100
