92a8118d8deaa9dee94838483d4361144d023cb2
zaddyphwithafatty <enzo.marin@ucas-edu.net>
2025-09-08 15:55:13 -0600

    fixed calc assingment

--- practices/budget_calc.py
+++ practices/budget_calc.py
@@ -1,27 +1,22 @@
 # EM 7th calc
 
 # - income, rent, utlities, groceries, transportation saving is 10
-spend = income -(utilities +rent -  groceries + traonsportation + savings)
-precent_rent = incomen * 100
-monthly_income = 3000
-mortgage = 1200
-monthly_utilities = 200
-monthly_groceries = 250
-transportation = 500
+
+
 #ask the user for their income and expenses
-income = float(input("hi ould you tell me wat is your monhly income?"))
+income = float(input("Hi could you tell me what is your monhly income?"))
 
 rent = float(input("what is your monthly rent?"))
-utilities = float(input("what ar your monthly utiltity bills?"))
+utilities = float(input("what is your monthly utiltity bills?"))
 groceries = float(input("how much do you spend on groceries each month?"))
-trasportation = float(input("how much doyou spend on transportation each month?"))
-
+transportation = float(input("how much doyou spend on transportation each month?"))
+savings = income * 0.1
 #calculat percenages of income
-
+spend = income -(utilities +  rent -  groceries + transportation + savings)
 rent_percent = (rent / income) * 100 
 utilities_percent = (utilities / income) * 100
 groceries_percent = (groceries / income) * 100
-transportation_income = (trasportation / income) * 100
+transportation_precent  = (transportation / income) * 100
 
 #calculate savings (10% of income)
 
@@ -30,14 +25,14 @@ saving_percent = 10
 
 #calculate reamining money
 
-total_expenses = rent + utilities + groceries + trasportation + saving
+total_expenses = rent + utilities + groceries + transportation + saving
 leftover = income - total_expenses
 
 #print the results 
 
-print("your rent is $ ", rent " and that is", round(rent_percent), "% of your income")
-print("your utilities are $", utilities, "and that is", round(utilities_percent), "% of your income")
-print("your groceries $", groceries, "and tht is", round (groceries_percent), "% of your income")
-print("your transportation is $ ", trasportation " and that is", round (transportation_percent), "% of your income")
-print("you should save $", round( savings, 2), "each month, that is", saving_percent, "% of your income")
-print("you have $", round (leftover, 2), "0f spending money each month")
+print ("your rent is $ ", rent, " and that is", round(rent_percent, 2), "% of your income")
+print ("your utilities are $", utilities, "and that is", round(utilities_percent), "% of your income")
+print ("your groceries $", groceries, "and tht is", round (groceries_percent), "% of your income")
+print ("your transportation is $ ", transportation, " and that is", round(transportation_percent), "% of your income")
+print ("you should save $", round( saving, 2), "each month, that is", saving_percent, "% of your income")
+print ("you have $", round (leftover, 2), "0f spending money each month")
