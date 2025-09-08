5721ae4f8380d3060e7112b6e8c685e060f656cb
zaddyphwithafatty <enzo.marin@ucas-edu.net>
2025-09-07 19:30:01 -0600

    budget_calc.py

--- practices/budget_calc.py
+++ practices/budget_calc.py
@@ -1,5 +1,10 @@
 # EM 7th calc
 
 # - income, rent, utlities, groceries, transportation saving is 10
-# spend = income -(utilities +rent -  groceries + traonsportation + savings)
+spend = income -(utilities +rent -  groceries + traonsportation + savings)
 precent_rent = incomen * 100
+monthly_income = 3000
+mortgage = 1200
+monthly_utilities = 200
+monthly_groceries = 250
+transportation = 500
