8e92c9ffead06f24c7f2ddb53aa4261d1ad32f8a
zaddyphwithafatty <enzo.marin@ucas-edu.net>
2025-09-10 14:41:40 -0600

    silly sentence hw

---
+++ silly_sentence.py
@@ -0,0 +1,7 @@
+# EM 7th silly sentence
+
+name = input("give me a silly name : \n")
+adjective = input("give me an adjective :\n")
+verb = input ("give me a verb :\n")
+
+print(f"{name} is {adjective} and is {verb}")
