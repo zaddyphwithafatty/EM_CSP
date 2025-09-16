d7f7717af5b9d344671637aa77433c4657f1fbde
zaddyphwithafatty <enzo.marin@ucas-edu.net>
2025-09-16 14:38:31 -0600

    i did hello update

---
+++ practices/update_hello.py
@@ -0,0 +1,12 @@
+#EM 7th update hello world
+
+
+def hello(name): 
+    print(f"hello {name}") 
+
+name = input("what is your name/m:")
+hello("enzo")
+hello("hannha")
+hello("isaiah")
+hello("gyan")
+hello("eva")
