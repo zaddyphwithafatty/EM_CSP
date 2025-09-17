1134d5b798f2c99e12f2124a18715b699634cf48
zaddyphwithafatty <enzo.marin@ucas-edu.net>
2025-09-17 14:41:56 -0600

    conditionals notes

---
+++ notes/conditionals_notes.py
@@ -0,0 +1,32 @@
+# Em 7th conditional notes 
+
+#What puts something inside of the “if” statement?
+homework_done = input("is your homework done?")
+homework_done = "yes" 
+if homework_done == "no":
+    print("Then go do your homework!")
+else:
+    print("yes you can go")
+
+greade = input("what is your grade?")
+
+grade = 100
+if grade > 89:
+    print(f"you have{grade} thats an A")
+elif grade > 79:
+    print(f"you have {grade}% thats a B!")
+
+else:
+    print(f"you have {grade}%! thats a C or lower")
+
+
+#What do if statements do?
+# to check if something is true or false
+#What are boolean statements? 
+
+#What do else statements do?
+
+#What kind of statement do you use if you have more than 2 needed outcomes?
+
+#What do each of the different symbols mean in conditionals?
+
