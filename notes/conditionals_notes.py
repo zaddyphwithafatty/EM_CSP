a7aa75fb717addac2eef8efdcd1fdf4b829e6ae9
zaddyphwithafatty <enzo.marin@ucas-edu.net>
2025-09-18 14:23:27 -0600

    condition notes

--- notes/conditionals_notes.py
+++ notes/conditionals_notes.py
@@ -1,6 +1,6 @@
 # Em 7th conditional notes 
 
-#What puts something inside of the “if” statement?
+#What puts something inside of the “if” statement? tab
 homework_done = input("is your homework done?")
 homework_done = "yes" 
 if homework_done == "no":
@@ -8,7 +8,7 @@ if homework_done == "no":
 else:
     print("yes you can go")
 
-greade = input("what is your grade?")
+grade = input("what is your grade?")
 
 grade = 100
 if grade > 89:
@@ -19,14 +19,33 @@ elif grade > 79:
 else:
     print(f"you have {grade}%! thats a C or lower")
 
+#logical operatiors and, or not
 
-#What do if statements do?
+
+name = input("what is your name: ")
+
+if name == "ms Larose":
+    print("you are the teacher!")
+elif name == "tia" or name == "ashley": 
+    print("you are the TA")
+else:
+    print(f"hello {name}, you are a student!")
+
+
+#What do if statements do? check if something is true or false
 # to check if something is true or false
-#What are boolean statements? 
+#What are boolean statements? alwsay true or false
+#What do else statements do?specifies a block of code
 
-#What do else statements do?
+#What kind of statement do you use if you have more than 2 needed outcomes? elif
 
-#What kind of statement do you use if you have more than 2 needed outcomes?
-
-#What do each of the different symbols mean in conditionals?
-
+#What do each of the different symbols mean in conditionals? 
+#< less than
+#> greater than
+#<= less than or equal 
+#>= greater or equal to
+#== are they equal
+#!not equal to
+#What are the 3 logical operators? and or not
+#What are logical operators for? can be usd to mka edecision in code or evalute conplex statements
+#What does a nested conditional statement do? a condition that only happens inside anothe rone
