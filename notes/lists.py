3c4340bfa068c42bd37af39748e9c9e104dd5dbc
zaddyphwithafatty <enzo.marin@ucas-edu.net>
2025-09-19 14:41:48 -0600

    time

---
+++ notes/lists.py
@@ -0,0 +1,5 @@
+# EM 7th lists notes
+
+#lists are surraundedd by brakets and are seperated by cotation marks insie the brakets 
+
+siblings = ["Vicente"]
---
+++ practices/time.py
@@ -0,0 +1,13 @@
+# em 7th time 
+
+import datetime
+
+now = datetime.datetime.now()
+hour = now. hour
+
+print(f"the hour is: {hour}")
+
+if hour >= 19:
+    print("good evening")
+
+elif
