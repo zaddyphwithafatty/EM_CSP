018be36dff394437d3f4aa6610ead9637e401b4e
zaddyphwithafatty <enzo.marin@ucas-edu.net>
2025-09-19 14:04:21 -0600

    time

---
+++ notes/time_notes.py
@@ -0,0 +1,18 @@
+#Em 7th time notes
+
+import time
+import datetime
+
+epoch = time.time()
+readable_time = time.time(epoch)
+
+
+
+
+print(f"the time in seconds since jan 1, 1970: {epoch}")
+print(f"the time is: {readable_time}")
+
+now = datetime.datetime.now()
+hour = now. hour
+print(f"the time is{now}")
+print(f"the hour is: {hour}")
