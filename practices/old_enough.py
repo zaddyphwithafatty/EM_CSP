cffc901906b65ed525f76695ac8f5c469e7a0311
zaddyphwithafatty <enzo.marin@ucas-edu.net>
2025-09-22 16:12:48 -0600

    old enough to what

--- practices/old_enough.py
+++ practices/old_enough.py
@@ -5,3 +5,11 @@ age = int(input("how old are you"))
 if age <= 4:
     print("you can go to school") 
     
+elif age <= 16:
+    print("you are able to drive")
+
+elif age < 15:
+    print("you are ale to get your lernenrs permit")
+
+elif age <= 21:
+    print("you are able to vote")
