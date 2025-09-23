e3f520ac1c9e970f9dc7756e7322ec00b6d9e1de
zaddyphwithafatty <enzo.marin@ucas-edu.net>
2025-09-23 14:32:39 -0600

    fizzbuzz

--- practices/fizzbuzz.py
+++ practices/fizzbuzz.py
@@ -1,4 +1,13 @@
-#fizzbuzz 
-
-for x in range(1,51):
-    print(x)
+#EM 7th fizzbuzz 
+x = 1
+while x <= 50:
+        if x % 3 ==0 and x % 5 ==0:
+               print("FizzBuzz")
+        elif x % 5 ==0:
+               print("Buzz!")
+        elif x % 3 ==0:
+               print("Fizz!")
+        else:
+               print(x)
+        x+=1
+print("Thats all numbers through 50")
