c9e287199dd20a53aa76fef766cfc13263137614
zaddyphwithafatty <enzo.marin@ucas-edu.net>
2025-10-06 20:51:27 -0600

    Update variables_practice.c

--- c_programming_2/practice/variables_practice.c
+++ c_programming_2/practice/variables_practice.c
@@ -2,6 +2,18 @@
 #include <stdio.h>
 
 int main(void){
-    char name = "enzo";
-    int lucky_number
+    char name[] = "Enzo";
+    int lucky_number = 15;
+    float decimal_number = 67.41;
+    char breakfast[] = "water and gum";
+    char_favorite color[] = "dark green"
+    char school_name [] = "UCAS"
+    int current_year = 2025;
+    char eye_color [] = " dark brown"
+    int age = 14
+    char favorite_subject [] = "world civilazation"
+
+    printf("my name is %s\n, name)
+    printf(my lucky number is %d\n, lucky_number)
+    printf("a decimal number is %f\n, decimal_number);
 }
