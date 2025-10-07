74959b3e07c2731cdfefb00095427ad55dd4abab
zaddyphwithafatty <enzo.marin@ucas-edu.net>
2025-10-06 21:22:55 -0600

    Update variables_practice.c

--- c_programming_2/practice/variables_practice.c
+++ c_programming_2/practice/variables_practice.c
@@ -13,7 +13,14 @@ int main(void){
     int age = 14
     char favorite_subject [] = "world civilazation"
 
-    printf("my name is %s\n, name)
-    printf(my lucky number is %d\n, lucky_number)
-    printf("a decimal number is %f\n, decimal_number);
+    printf("my name is %s\n", name);
+    printf(my lucky number is %d\n", lucky_number);
+    printf("a decimal number is %f\n", decimal_number);
+    printf("what i had for breakfast was %s\n", breakfast);
+    printf("my favorite color is %s\n", favorite_color);
+    printf("i go to %s\n", school_name);
+    printf("the year is %d\n", current_year);
+    printf("my eye color is%s\n", eye_color);
+    printf(im %d years old\n", age);
+    printf("my favorite subject is %s\n," favorite subject);
 }
