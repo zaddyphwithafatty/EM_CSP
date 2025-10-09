0fa00d086f0c865aa03c1c9818de8aa36a8cbdf6
zaddyphwithafatty <enzo.marin@ucas-edu.net>
2025-10-08 18:12:18 -0600

    Create expressions_practice.c

---
+++ c_programming_2/practice/expressions_practice.c
@@ -0,0 +1,17 @@
+// EM 7th expression practice
+# include <stdio.h>
+#include Math.h
+
+int main() {
+  printf("%d\n", 7-24/8*4+6);
+  printf("%d", 18/3-+2*5);
+  printf("%d", 6*4/12+72/8-9);
+  printf("%d", (17-6/2)+4*3);
+  printf("%d", -2(1*4-2/2)+(6++2-3));
+  printf("%d", -1**[(3-4*7)/5]-2*24/6)
+  printf("%d", (3*pow(5,2)/15)-(5-pow(2,2)
+  printf("%d", (pow(1,4)*pow(2,2)+pow(3,3))-pow(2,5)/4
+  printf("%d",pow((22/2-2*5,2))+pow((4-6/6,2))
+
+  return 0;
+}
