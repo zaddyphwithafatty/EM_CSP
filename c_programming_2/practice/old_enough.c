d25ed6da1a108398027dd297c717ccfa8f9fd167
zaddyphwithafatty <enzo.marin@ucas-edu.net>
2025-10-10 17:39:44 -0600

    Update old_enough.c

--- c_programming_2/practice/old_enough.c
+++ c_programming_2/practice/old_enough.c
@@ -10,8 +10,12 @@ int main(void) {
     if(age >= 18){
         printf("you are allowed to vote");
     }else if(age >= 16){
-        printf("you are allowed to drive by yourself")
-
-
+        printf("you are allowed to drive by yourself");
+    }else if(age >= 15){
+        printf("you are allowed to get your learners permit");
+    }else if(age >= 4){
+        printf(" GO TO SCHOOL!!!");
+    }
+    
     return 0;
 }
