85327266a9be356888bd38d3f0d7d78e8db174c3
zaddyphwithafatty <enzo.marin@ucas-edu.net>
2025-10-12 17:39:57 -0600

    Create silly_sentence.py

---
+++ c_programming_2/practice/silly_sentence.py
@@ -0,0 +1,21 @@
+// em 7th silly sentence
+#include <stdio.h>
+#include <string.h>
+
+main(void){
+  char noun[20];
+  printf("type in a noun");
+  scanf("%s", noun);
+
+  char color[20];
+  printf("give me your favorite color")
+  scanf("%s", color);
+
+  char food[20]
+  printf("what is your favorite food to eat");
+  scanf("%s", food);
+
+  printf("as the person saw 
+
+  return 0;
+}
