81c48f879e3fe48db36a9fd5ee2427be75152b2a
zaddyphwithafatty <enzo.marin@ucas-edu.net>
2025-10-14 02:21:07 +0000

    fizzbuzz

--- c_programming_2/practice/fizzbuzz.c
+++ c_programming_2/practice/fizzbuzz.c
@@ -1,3 +1,20 @@
 // em 7th fizzbuzz
 #include <stdio.h>
 
+int main (void){
+    int num = 1;
+    while(num <= 50){
+        printf("%d\n", num);
+} if(num / 3,= 0);{
+    printf("fizz\n");
+
+} if(num / 5, = 0);{
+    printf("buzz");
+    
+} if (num / 5,3 =0);{
+    printf("fizzbuzz");
+}
+
+    num++;
+
+}
