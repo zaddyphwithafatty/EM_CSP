9f5d49255a008538ebc28cc0a154a8c830002f02
zaddyphwithafatty <enzo.marin@ucas-edu.net>
2025-10-15 00:24:51 +0000

    www

--- c_programming_2/practice/fizzbuzz.c
+++ c_programming_2/practice/fizzbuzz.c
@@ -4,16 +4,18 @@
 int main (void){
     int num = 1;
 
-    for (int num =1; num <=50; num++){
-        if(num % 3 ==0 );
-            pritnf("fizz\n");
-    }   
-        else if (num % 5==0){
-            printf("buzz")
-
-    }
-
-        else if (num )
+    for(int num=1; num<=50; num++){
+        if(num % 3 ==0){
+            printf("fizz\n");
+        }
+        else if(num % 5 == 0){
+            printf("buzz\n");
+        }
+        else if(num % 3 == 0 && num % 5 ==0){
+            printf("fizzbuzz\n");
+        }
+       } 
+       num++;
 
     return 0; 
 }
