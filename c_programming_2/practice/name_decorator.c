2f10b2aae85df8f213215ff9887b6fcb6ef4a18b
zaddyphwithafatty <enzo.marin@ucas-edu.net>
2025-10-18 22:48:47 -0600

    Update name_decorator.c

--- c_programming_2/practice/name_decorator.c
+++ c_programming_2/practice/name_decorator.c
@@ -1,11 +1,17 @@
 // em 7th name decorator
 #include <stdio.h>
+#include <string.h>
 
 int main(void){
-  char name[30];
-  printf("what is your name:\n");
-  strcat(name, ("*** ***"));
-  printf("%s", name);
+  char name[20];
+  printf("what is your name");
+  scanf("%s", name);
+  while (getcahr() != '\n');
 
+  char decorated name[40] = "$$%$%^";
+  strcat(decorated_name, name);
+  strcat(decorated_name, " @$&%#&");
+  printf("%s\n", decorated_name);
 
+return 0;
 }
